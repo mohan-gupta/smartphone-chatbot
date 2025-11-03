313f28bf00994a7ae04325626dd9fd4377f25884
Mohan Gupta <mohankrishnagupta@gmail.com>
2025-11-04 01:03:04 +0530

    modified chainlit config

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from chainlit.utils import mount_chainlit
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    with open("chaiblit_config.py") as f:
-        content = f.read()
-        
-    with open("/var/task/_vendor/chainlit/config.py", "w") as f:
-        f.write(content)
-
-app = FastAPI(lifespan=lifespan)
+    
+app = FastAPI()
 
 
 @app.get("/app")
 def read_main():
-    return {"message": "Hello World from main app"}
+    return {"message": "Welcome to Smartphone buying Agent!"}
+
+
+
+with open("chaiblit_config.py") as f:
+        content = f.read()
+        
+with open("/var/task/_vendor/chainlit/config.py", "w") as f:
+    f.write(content)
 
 mount_chainlit(app=app, target="/tmp/cl_app.py", path="/chainlit")
