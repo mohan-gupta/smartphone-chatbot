8374a516878fa6a5d64ab42035810dab7b708316
Mohan Gupta <mohankrishnagupta@gmail.com>
2025-11-04 01:07:00 +0530

    resolved chainlit config issue

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from contextlib import asynccontextmanager
+with open("chainlit_config.py") as f:
+        content = f.read()
+        
+with open("/var/task/_vendor/chainlit/config.py", "w") as f:
+    f.write(content)
 
 from fastapi import FastAPI
 from chainlit.utils import mount_chainlit
@@ -10,12 +14,5 @@ app = FastAPI()
 def read_main():
     return {"message": "Welcome to Smartphone buying Agent!"}
 
-
-
-with open("chainlit_config.py") as f:
-        content = f.read()
-        
-with open("/var/task/_vendor/chainlit/config.py", "w") as f:
-    f.write(content)
 
 mount_chainlit(app=app, target="/tmp/cl_app.py", path="/chainlit")
